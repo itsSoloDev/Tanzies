df19cf2e0910d8c411ec4ea9b47ce5f0edb7266c
anurag <anuragrawat92003@gmail.com>
2024-08-25 15:51:07 +0530

    dice elements.

---
+++ Tenzies/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import Die from './components/Die'
+import './App.css'
+import { nanoid } from 'nanoid';
+
+export default function App(){
+
+  const [dice,setDice] = useState(allNewDice());
+
+  function allNewDice(){
+    let newDice=[];
+    for(let i=0;i<10;i++){
+        newDice.push({
+          value:Math.ceil(Math.random()*6),
+          isHeld:false,
+          id:nanoid()
+        })
+    }
+    return newDice;
+  }
+
+  function rollDice(){
+    setDice(allNewDice())
+  }
+
+  function holdDice(id){
+    setDice(oldDice=>{
+      oldDice.map(die=>{
+        return die.id===id ? {...die , isHeld:!die.isHeld}: die
+      })
+    })
+  }
+
+  const diceElements = dice.map(die=>(
+    <Die 
+    key={die.id} 
+    value={die.value} 
+    isHeld={die.isHeld} 
+    holdDice={()=>holdDice(die.id)} 
+     />
+  ))
+
+  return(
+    <main>
+     <div className="dice-container">
+      {diceElements}
+     </div>
+     <button className='roll-dice' onClick={rollDice}>Roll</button>
+    </main>
+  )
+}
+
